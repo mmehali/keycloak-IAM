629e62d5a68c2a5e5e4daf9ba21a7f400d7c1686
Sofiane <mokhtar.mehali@gmail.com>
2021-01-15 16:50:50 +0100

    modif 1

--- Install/bootstrap.sh
+++ Install/bootstrap.sh
@@ -7,8 +7,8 @@ POSTGRESQL_URL=https://jdbc.postgresql.org/download/postgresql-$POSTGRES_VERSION
 
 
 sudo yum check-update 
-sudo yum clean all
-sudo yum update 
+sudo yum clean all -y
+sudo yum update -y
 
 
 sudo yum install -y wget
@@ -23,8 +23,8 @@ sudo yum install -y java-1.8.0-openjdk
 echo "--------------------------------------------------"
 echo "Step 5: Creer  user/group keycloak pour keycloak "
 echo "--------------------------------------------------"
-sudo groupadd -r keycloak
-sudo useradd  -r -g keycloak -d /opt/keycloak -s /sbin/nologin keycloak
+#sudo groupadd -r keycloak
+#sudo useradd  -r -g keycloak -d /opt/keycloak -s /sbin/nologin keycloak
 
 
 echo "--------------------------------------------"
@@ -53,13 +53,13 @@ sudo tar xfz /vagrant/downloads/keycloak-${KEYCLOAK_VERSION}.tar.gz -C /opt
 echo "--------------------------------------------------------------------"
 echo "Step 4: create a lien symbolique pointant sur le rep d'installation "
 echo "--------------------------------------------------------------------"
-sudo ln -s /opt/keycloak-${KEYCLOAK_VERSION} /opt/keycloak
+test -d /opt/keycloak-${KEYCLOAK_VERSION} && sudo ln -s /opt/keycloak-${KEYCLOAK_VERSION} /opt/keycloak
 
 
 echo "-----------------------------------------------------"
 echo "Step 6: donner l'acces (exec) au user/groug keycloak "
 echo "-----------------------------------------------------"
-sudo chown -RH keycloak: /opt/keycloak
+#sudo chown -RH keycloak: /opt/keycloak
 
 
 
@@ -77,12 +77,11 @@ then
     echo "Installation postgresql depuis /vagrant/downloads/postgresql-${POSTGRES_VERSION}.jar..."
 else
     echo "Téléchargement de  postgresql-${POSTGRES_VERSION}.jar ..."
-    mkdir -p /vagrant/downloads
-    wget -q -O /vagrant/downloads/postgresql-${POSTGRES_VERSION}.jar "${POSTGRES_URL}"
+    wget -q -O /vagrant/downloads/postgresql-${POSTGRES_VERSION}.jar  "${POSTGRES_URL}"
     if [ $? != 0 ];
     then
         echo "GRAVE: Téléchargement du driver Postgres impossible depuis ${POSTGRES_URL}"	
-        exit 1
+        #exit 1
     fi
     echo "Installation du driver postgres ..."
 fi
@@ -90,39 +89,39 @@ fi
 echo "------------------------------------------------"
 echo "Step 9 : installation du driver postgres        "
 echo "------------------------------------------------" 
-mkdir -p /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main
-cd /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main
-sudo cp /vagrant/downloads/postgresql-${POSTGRES_VERSION}.jar .
-cp /vagrant/postgres/module.xml .
+sudo mkdir -p /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main
+sudo cd /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main/
+sudo cp /vagrant/downloads/postgresql-${POSTGRES_VERSION}.jar /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main/
+sudo cp /vagrant/postgres/module.xml /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main/
 
 echo "-----------------------------------------------------"
-echo "Step 9 : configuration keycloak                      "
+echo "Step 10 : configuration keycloak                      "
 echo "-----------------------------------------------------"
 
 sudo /opt/keycloak/bin/jboss-cli.sh --file=/vagrant/standalone-ha-config.cli
 sudo rm -rf /opt/keycloak/standalone/configuration/standalone_xml_history
 
 echo "-----------------------------------------------------"
-echo "Step 10: ajouter un administateur                    "
+echo "Step 11: ajouter un administateur                    "
 echo "-----------------------------------------------------"
-/opt/keycloak/bin/add-user-keycloak.sh -u admin -p admin
+sudo /opt/keycloak/bin/add-user-keycloak.sh -u admin -p admin
 
 
 echo "-----------------------------------------------------"
-echo "Step 10: Configure keycloak to be run as a service   "
+echo "Step 12: Configure keycloak to be run as a service   "
 echo "-----------------------------------------------------"
 sudo mkdir -p /etc/keycloak
 
-sudo cp /vagrant/launch.sh /opt/keycloak-${KEYCLOAK_VERSION}/bin/
-sudo sh -c 'chmod +x /opt/keycloak-${KEYCLOAK_VERSION}/bin'
-#sudo chown keycloak: /opt/keycloak-${KEYCLOAK_VERSION}/bin/launch.sh
+sudo cp /vagrant/service/launch.sh /opt/keycloak/bin/
+sudo sh -c 'chmod +x /opt/keycloak/bin'
+#sudo chown keycloak: /opt/keycloak/bin/launch.sh
 
-sudo cp /vagrant/keycloak.service /etc/systemd/system/
+sudo cp /vagrant/service/keycloak.service /etc/systemd/system/
 
-#sudo cp /vagrant/keycloak.conf /etc/keycloak-${KEYCLOAK_VERSION}/
+sudo cp /vagrant/service/keycloak.conf /etc/keycloak/
 
 echo "-----------------------------------------------------"
-echo "Step 11: start the  keycloak service"
+echo "Step 12: start the  keycloak service"
 echo "-----------------------------------------------------"
 sudo systemctl daemon-reload
 sudo systemctl start keycloak
@@ -132,7 +131,7 @@ sudo systemctl enable keycloak
 
 #sudo /sbin/service keycloak start
 echo "-----------------------------------------------------"
-echo "Step 11: Opening port 8080 on iptables ...           "
+echo "Step 13: Opening port 8080 on iptables ...           "
 echo "-----------------------------------------------------"
 iptables -I INPUT 3 -p tcp -m state --state NEW -m tcp --dport 8080 -j ACCEPT
 iptables-save > /etc/sysconfig/iptables
