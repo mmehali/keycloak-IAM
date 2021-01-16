5cc388480d06a7e4f0d4896356e2a47629a06436
Sofiane <mokhtar.mehali@gmail.com>
2021-01-16 16:56:12 +0100

    conf qui fonstionne avec root

--- Install/bootstrap.sh
+++ Install/bootstrap.sh
@@ -5,7 +5,9 @@ POSTGRES_VERSION=42.2.18
 KEYCLOAK_URL=http://downloads.jboss.org/keycloak/${KEYCLOAK_VERSION}/keycloak-${KEYCLOAK_VERSION}.tar.gz
 POSTGRESQL_URL=https://jdbc.postgresql.org/download/postgresql-$POSTGRES_VERSION.jar
 
-
+echo "---------------------------------------------"
+echo " Etape 0 : Mise a jour des package           "
+echo "---------------------------------------------"
 sudo yum check-update 
 sudo yum clean all -y
 sudo yum update -y
@@ -28,7 +30,7 @@ echo "--------------------------------------------------"
 
 
 echo "--------------------------------------------"
-echo "Step 2 : Telecharger keycloak               "
+echo "Step 2 : Telechargement de keycloak         "
 echo "--------------------------------------------"
 if [ -f "/vagrant/downloads/keycloak-${KEYCLOAK_VERSION}.tar.gz" ];
 then
@@ -45,21 +47,25 @@ else
     echo "Installation Keycloak ..."
 fi
 
-echo "------------------------------------------------"
-echo "Step 3 : Extraire keycloak tar.gz dans /opt "
-echo "------------------------------------------------" 
-sudo tar xfz /vagrant/downloads/keycloak-${KEYCLOAK_VERSION}.tar.gz -C /opt
+
+
+echo "------------------------------------------------------------------"
+echo "Step 3 : Extraction keycloak-${KEYCLOAK_VERSION}.tar.gz dans /opt "
+echo "------------------------------------------------------------------" 
+sudo tar xvfz /vagrant/downloads/keycloak-${KEYCLOAK_VERSION}.tar.gz -C /opt
+
+
 
 echo "--------------------------------------------------------------------"
-echo "Step 4: create a lien symbolique pointant sur le rep d'installation "
+echo "Step 4 : create a lien symbolique pointant sur le rep d'installation "
 echo "--------------------------------------------------------------------"
-test -d /opt/keycloak-${KEYCLOAK_VERSION} && sudo ln -s /opt/keycloak-${KEYCLOAK_VERSION} /opt/keycloak
+test -d /opt/keycloak-${KEYCLOAK_VERSION} || sudo ln -s /opt/keycloak-${KEYCLOAK_VERSION} /opt/keycloak
 
 
 echo "-----------------------------------------------------"
-echo "Step 6: donner l'acces (exec) au user/groug keycloak "
+echo "Step 5 : donner l'acces (exec) au user/groug keycloak "
 echo "-----------------------------------------------------"
-#sudo chown -RH keycloak: /opt/keycloak
+#sudo chown -R keycloak:keycloak /opt/keycloak
 
 
 
@@ -68,6 +74,8 @@ echo "Step 7 : Limiter l'acces au repertoire standalone "
 echo "--------------------------------------------------" 
 #sudo -u keycloak chmod 700 /opt/keycloak/standalone
 #sudo chmod 777 /opt/keycloak/standalone
+
+
 
 echo "-----------------------------------------------------"
 echo "Step 8 : Telecharger le driver postgresql            " 
@@ -94,11 +102,14 @@ sudo cd /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main/
 sudo cp /vagrant/downloads/postgresql-${POSTGRES_VERSION}.jar /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main/
 sudo cp /vagrant/postgres/module.xml /opt/keycloak/modules/system/layers/base/org/postgresql/jdbc/main/
 
+
+
 echo "-----------------------------------------------------"
 echo "Step 10 : configuration keycloak                     "
 echo "-----------------------------------------------------"
 sudo /opt/keycloak/bin/jboss-cli.sh --file=/vagrant/standalone-ha-config.cli
 sudo rm -rf /opt/keycloak/standalone/configuration/standalone_xml_history
+
 
 echo "-----------------------------------------------------"
 echo "Step 11: ajouter un administateur keycloak           "
@@ -107,27 +118,44 @@ sudo /opt/keycloak/bin/add-user-keycloak.sh -u admin -p admin
 
 
 echo "-----------------------------------------------------"
-echo "Step 12: Configure keycloak to be run as a service   "
+echo "Step 12: Configuration systemD                       "
 echo "-----------------------------------------------------"
-sudo mkdir -p /etc/keycloak
 
+echo "Step 12.1 : Copier de config keycloak.conf dans /etc/keycloak"
+sudo mkdir -p /etc/keycloak
+sudo cp /vagrant/service/keycloak.conf /etc/keycloak/
+sudo more /etc/keycloak/keycloak.conf
+
+echo "Step 12.2 : Copier et configurer le fichier de demarrage lauch.sh"
 sudo cp /vagrant/service/launch.sh /opt/keycloak/bin/
-sudo sh -c 'chmod +x /opt/keycloak/bin'
+sudo more /opt/keycloak/bin/lauch.sh
+sudo more | ll /opt/keycloak/bin/lauch.sh
+sudo chmod +x /opt/keycloak/bin/launch.sh
 #sudo chown keycloak: /opt/keycloak/bin/launch.sh
 
+echo "Step 12.3 : Copier et configurer le fichier de service keycloak.service"
 sudo cp /vagrant/service/keycloak.service /etc/systemd/system/
+more /etc/systemd/system/keycloak.service
 
-sudo cp /vagrant/service/keycloak.conf /etc/keycloak/
 
 echo "-----------------------------------------------------"
-echo "Step 12: start the  keycloak service"
+echo "Step 13: demarrage du service keycloak               "
 echo "-----------------------------------------------------"
-sudo systemctl daemon-reload
+#sudo systemctl daemon-reload
 sudo systemctl start keycloak
 sudo systemctl enable keycloak
+
+echo "-----------------------------------------------------"
+echo "Step 14: etat du service keycloak                    "
+echo "-----------------------------------------------------"
 sudo systemctl status keycloak
-sudo tail -f /opt/keycloak/standalone/log/server.log
-journalctl -u keycloak.service
+
+echo "-----------------------------------------------------"
+echo "Step 14: logs du server keycloak                     "
+echo "-----------------------------------------------------"
+
+#sudo tail -f /opt/keycloak/standalone/log/server.log
+#journalctl -u keycloak.service
 
 #voir https://medium.com/@hasnat.saeed/setup-keycloak-server-on-ubuntu-18-04-ed8c7c79a2d9
 
