f8743ed32a01d1603a7e116eba30c9f8b9039781
mmehali <34216226+mmehali@users.noreply.github.com>
2021-01-25 12:55:47 +0100

    Delete docker-build.sh

--- keycloak/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#KC_VERSION=${KC_VERSION:-latest}
-KC_VERSION=${KC_VERSION:-4.5.0.Final.1.wjax}
-
-docker build -t tdlabs/keycloak:$KC_VERSION .
-
-docker images -f "dangling=true" -q | xargs --no-run-if-empty docker rmi -f
