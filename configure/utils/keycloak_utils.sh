230dac2409b0e72898933f94dfc0f8a752ef697b
mmehali <34216226+mmehali@users.noreply.github.com>
2021-01-28 17:06:34 +0100

    Update keycloak_utils.sh

--- configure/utils/keycloak_utils.sh
+++ configure/utils/keycloak_utils.sh
@@ -2,18 +2,6 @@
 
 set -euo pipefail
 
-# creation d'un nouveau realm et l'activé.
-# le realm n'est creer que s'il n'exite pas.
-createRealm() {
-  # arguments
-  REALM_NAME=$1
-  
-  REALM_EXIST=$($KCADM get realms/$REALM_NAME)
-  if [ "$REALM_EXIST" == "" ]; then
-    $KCADM create realms -s realm="${REALM_NAME}" -s enabled=true
-  fi
-}
-
 # creation d'un nouveau client et l'activé.
 # le client n'est creer que s'il n'exite pas.
 createClient() {
